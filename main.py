d1fd7a022c3e78864691ee775826c48bfbc6ae44
Sumedh Girish <sumedh.girish@gmail.com>
2025-07-16 16:10:27 +0530

    Blinking Lights

---
+++ main.py
@@ -0,0 +1,15 @@
+import gpiozero as gpio
+from signal import pause
+
+def main():
+    print("Hello from piware!")
+
+    # testing some cool stuff
+
+    led = gpio.LED(18);
+    led.blink()
+    pause()
+
+
+if __name__ == "__main__":
+    main()
