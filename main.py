42072c33338e9c405964d85ea3ee8d8a8fe691b6
Sumedh Girish <sumedh.girish@gmail.com>
2025-07-16 16:39:22 +0530

    Trying to make it work by removing pin shenanigans

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import gpiozero as gpio
 from signal import pause
 
-from gpiozero.output_devices import PiGPIOFactory
-
-gpio.Device.pin_factory = PiGPIOFactory("127.0.0.1", "8888")
-
-
 def main():
     print("Hello from piware!")
 
     # testing some cool stuff
-
     led = gpio.LED(18);
     led.blink()
     pause()
