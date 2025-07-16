7a5f51a0127e0b528fc35a328ea75defbeab9b80
Sumedh Girish <sumedh.girish@gmail.com>
2025-07-16 17:01:16 +0530

    Trying to work with signals

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import gpiozero as gpio
+import signal
+from sys import argv
+
+if argv[1] and argv[1] == "Mock":
+    from gpiozero.pins.mock import MockFactory
+    gpio.Device.pin_factory = MockFactory()
+
+def IntHandler(*_):
+    print("Recieved interrupt, Exiting!")
 
 def main():
     print("Hello from piware!")
 
     # testing some cool stuff
     led = gpio.LED(18);
-    try:
-        led.blink()
-        while True:
-            pass
-    except (KeyboardInterrupt, EOFError):
-        print("Exiting")
+    led.blink()
+
+    signal.pause()
 
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, IntHandler)
     main()
+    signal.getsignal(signal.SIGINT)
