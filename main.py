4c6c565e1e2378824c9bb765493fa77351a957f0
Sumedh Girish <sumedh.girish@gmail.com>
2025-07-17 08:48:40 +0530

    Blinking Lights!

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import gpiozero as gpio
 import signal
 from sys import argv
 
-if argv[1] and argv[1] == "Mock":
-    from gpiozero.pins.mock import MockFactory
-    gpio.Device.pin_factory = MockFactory()
+stop = False
+
+match argv:
+    case [_, "mock"]:
+        from gpiozero.pins.mock import MockFactory
+        gpio.Device.pin_factory = MockFactory()
 
 def IntHandler(*_):
+    global stop
+    stop = True
     print("Recieved interrupt, Exiting!")
 
 def main():
+    global stop
     print("Hello from piware!")
 
     # testing some cool stuff
@@ -18,8 +24,10 @@ def main():
 
     signal.pause()
 
+    if stop:
+        led.off()
+
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, IntHandler)
     main()
-    signal.getsignal(signal.SIGINT)
